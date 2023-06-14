adaaebb86f0c0571d9fddd2ac295544e81211e22
MohammadNaiem65 <mohammadnaiem661065@gmail.comm>
2023-06-14 12:33:13 +0600

    assigned custom color and button style

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,17 @@ export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		extend: {
-			
+			fontFamily: {
+				Popins: ["Poppins", "sans-serif"],
+				Vollokorn: ["Vollkorn", "serif"],
+			},
+			colors: {
+				Primary: "#8fb4ff",
+				Secondary: "#ebf1ff",
+				Accent: "#010809",
+				Bg: "#f2f2f2",
+				"Clr-base": "#808080",
+			},
 		},
 	},
 	plugins: [],
