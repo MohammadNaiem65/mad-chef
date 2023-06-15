d7945b5c13ec75ae282d552c4b3435f970e5e9eb
MohammadNaiem65 <mohammadnaiem661065@gmail.comm>
2023-06-15 08:00:51 +0600

    made the header responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,26 @@
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo.png";
+import { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Header = () => {
-	return ( 
-		<div className="container px-0">
-			<div className='flex justify-between items-center px-12'>
-				<img src={logo} alt='Mad Chef logo' className='w-48' />
-				<div className='flex gap-x-6 font-semibold text-lg'>
+	const [navOptionsStatus, setNavOptionsStatus] = useState(false);
+	console.log(navOptionsStatus);
+	return (
+		<div className='md:container px-0'>
+			<div className='flex justify-between items-center px-12 relative'>
+				<img src={logo} alt='Mad Chef logo' className='w-32 md:w-48' />
+				<p className='text-xl block md:hidden absolute right-14 duration-300 cursor-pointer'>
+					{navOptionsStatus ? (
+						<FaTimes onClick={() => setNavOptionsStatus(!navOptionsStatus)} />
+					) : (
+						<FaBars onClick={() => setNavOptionsStatus(!navOptionsStatus)} />
+					)}
+				</p>
+				<div
+					className={`${
+						navOptionsStatus ? "top-24 bg-Secondary p-3 rounded" : "-top-28"
+					} font-semibold text-lg md:bg-transparent absolute right-14 md:static flex flex-col md:flex-row gap-x-6  transform md:translate-y-0 duration-300 ease-out`}>
 					<Link className='link-hover'>Home</Link>
 					<Link className='link-hover'>Favorite</Link>
 					<Link className='link-hover'>Blog</Link>
