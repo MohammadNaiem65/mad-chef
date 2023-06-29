ac5e74c3841e38fb4481fb4f9311f71b5a6c9aec
MohammadNaiem65 <mohammadnaiem661065@gmail.comm>
2023-06-29 17:40:17 +0600

    completed the slider design

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,18 +1,22 @@
-import React, { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import ReviewSlide from "../ReviewSlide/ReviewSlide";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa6";
 import Swiper, { Navigation, Pagination, Autoplay } from "swiper";
-import "swiper/swiper-bundle.css";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/autoplay";
 import "./Review.css";
 
 Swiper.use([Navigation, Pagination, Autoplay]);
 
 const Review = () => {
 	const swiperRef = useRef(null);
+	const [swiper, setSwiper] = useState(null);
 
 	useEffect(() => {
 		setTimeout(() => {
-			new Swiper(swiperRef.current, {
+			const mySwiper = new Swiper(swiperRef.current, {
 				// Optional parameters
 				loop: true,
 				speed: 250,
@@ -26,43 +30,41 @@ const Review = () => {
 					dynamicBullets: true,
 				},
 
-				// Navigation arrows
-				navigation: {
-					nextEl: ".swiper-button-next",
-					prevEl: ".swiper-button-prev",
-				},
+				modules: [Navigation, Pagination, Autoplay],
 			});
+
+			setSwiper(mySwiper);
 		}, 0);
 	}, []);
 
 	return (
-		<div className='w-3/5 h-96 mx-auto mt-14'>
-			<div
-				className='swiper-container relative overflow-hidden'
-				ref={swiperRef}>
-				<div className='swiper-wrapper'>
-					<div className='swiper-slide'>
-						<ReviewSlide num='1' />
-					</div>
-					<div className='swiper-slide'>
-						<ReviewSlide num='2' />
-					</div>
-					<div className='swiper-slide'>
-						<ReviewSlide num='3' />
-					</div>
+		<div
+			className='w-3/5 h-96 mx-auto mt-14 relative overflow-hidden'
+			ref={swiperRef}>
+			<div className='swiper-wrapper'>
+				<div className='swiper-slide'>
+					<ReviewSlide num='1' />
 				</div>
-
-				<div className='swiper-pagination'></div>
-
-				{/* <div className='swiper-button-next'><div> 
-				<div className='swiper-button-prev text-2xl'></div> */}
-				<div onClick={() => Swiper.navigation.prevEl()}>
-					<FaArrowLeft className='w-14 h-14 p-3 text-2xl text-Primary bg-Primary/20 hover:bg-Primary/50  absolute top-1/2 left-5 transform -translate-y-1/2 rounded-full cursor-pointer z-10' />
+				<div className='swiper-slide'>
+					<ReviewSlide num='2' />
 				</div>
-				<div onClick={() => Swiper.navigation.next()}>
-					<FaArrowRight className='w-14 h-14 p-3 text-2xl text-Primary bg-Primary/20 hover:bg-Primary/50  absolute top-1/2 right-5 transform -translate-y-1/2 rounded-full cursor-pointer z-10' />
+				<div className='swiper-slide'>
+					<ReviewSlide num='3' />
 				</div>
 			</div>
+
+			{/* Pagination */}
+			<div className='swiper-pagination'></div>
+
+			{/* Slider Control */}
+			<>
+				<div onClick={() => swiper && swiper.slidePrev()}>
+					<FaArrowLeft className='w-14 h-14 p-3 text-2xl text-Primary bg-Primary/20 hover:bg-Primary/50  absolute top-1/2 left-5 transform -translate-y-1/2 rounded-full cursor-pointer z-10' />
+				</div>
+				<div onClick={() => swiper && swiper.slideNext()}>
+					<FaArrowRight className='w-14 h-14 p-3 text-2xl text-Primary bg-Primary/20 hover:bg-Primary/50  absolute top-1/2 right-5 transform -translate-y-1/2 rounded-full cursor-pointer z-10' />
+				</div>
+			</>
 		</div>
 	);
 };
