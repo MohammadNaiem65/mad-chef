de4ca10e2312705233ac204422f24b8bd268076e
MohammadNaiem65 <mohammadnaiem661065@gmail.comm>
2023-07-02 23:50:48 +0600

    made the footer responsive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,24 +1,23 @@
-import React, { useState } from "react";
 import logo from "../../assets/logo.png";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
-	const [loggedIN, setLoggedIn] = useState(false);
 	return (
-		<div className='h-64 mt-16 px-[10%] bg-[#495579] text-stone-300 grid grid-cols-6'>
-			<div className='col-span-4'>
-				<img className='w-32 md:w-48' src={logo} alt='logo of mad chef' />
-				<p className='w-2/5 ml-3 text-sm font-Popins'>
+		<div className='md:h-64 mt-16 px-[5%] bg-[#495579] text-stone-300 md:flex'>
+
+			<div className=''> {/* Left Side Container */}
+				<img className='w-48' src={logo} alt='logo of mad chef' />
+				<p className='md:w-2/5 ml-3 text-sm font-Popins'>
 					Mad Chef is the perfect way to find and order food from countries best
 					chefs. Mad Chef can help you find the perfect meal for any occasion.
 				</p>
 			</div>
-			<div className='mt-10 mr-5 col-span-1'>
+
+			{/* Right side Containers */}
+			<div className='mt-10 mr-16'>
 				<h4 className='font-semibold font-Popins text-lg'>Useful links</h4>
 				<div
-					className={`mt-3 ${
-						loggedIN ? "top-24 bg-Secondary p-3 rounded" : "-top-28"
-					} font-Vollokorn text-lg md:bg-transparent absolute right-14 md:static flex flex-col transform md:translate-y-0 duration-300 ease-out`}>
+					className="mt-3 font-Vollokorn text-lg flex flex-col">
 					<Link className='w-fit link-hover'>Home</Link>
 					<Link className='w-fit link-hover'>Favorite</Link>
 					<Link className='w-fit link-hover'>Blog</Link>
@@ -28,7 +27,7 @@ const Footer = () => {
 					</p>
 				</div>
 			</div>
-			<div className='mt-10 mr-5 col-span-1'>
+			<div className='mt-10'>
 				<h4 className='font-semibold font-Popins text-lg'>Quick links</h4>
 				<div className='mt-3  font-Vollokorn text-lg flex flex-col'>
 					<Link className='w-fit link-hover'>Our Blog</Link>
